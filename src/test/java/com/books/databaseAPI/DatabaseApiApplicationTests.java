c010f3aa5d185866ad4f684a35d7a57a34dafa09
Francisco <63214442+frankymz@users.noreply.github.com>
2021-07-21 14:04:57 -0400

    Initial commit

---
+++ src/test/java/com/books/databaseAPI/DatabaseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.books.databaseAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
